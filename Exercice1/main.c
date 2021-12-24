2bcd01d033d95f054b49d954a5e2b38d57640e01
Tom <tommusique974@gmail.com>
2021-12-24 08:12:16 +0100

    Chercher les occurences d'un tableau

--- Exercice1/main.c
+++ Exercice1/main.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
 
-void saisie(int tab[])
-{
+void saisie(int tab[]) {
     int valeurs;
-    for (int i = 0; i < 10; i++)
-    {
-        printf("Saisir des nombres : ");
+    for (int i = 0; i < 10; i++) {
+        printf("Saisir des nombres :");
         scanf("%d", &valeurs);
         tab[i] = valeurs;
     }
 }
 
-int cherche(int tab1[], int tab2[])
-{
+void cherche(int tab1[], int tab2[]) {
     static int tab3[10] = {};
-    for (int i = 0; i < 10; i++)
-    {
-        if (tab1[i] == tab2[i])
-        {
+    for (int i = 0; i < 10; i++) {
+        if (tab1[i] == tab2[i]) {
             tab3[i] = tab1[i];
         }
     }
 
     printf("Valeur r%ccurente : \n", 130);
 
-    for (int i = 0; i < 10; i++)
-    {
-        if (tab3[i] != 0)
-        {
+    for (int i = 0; i < 10; i++) {
+        if (tab3[i] != 0) {
             printf("%d | ", tab3[i]);
         }
     }
 }
 
-int main()
-{
+int main() {
     int listeA[10];
     int listeB[10];
 
